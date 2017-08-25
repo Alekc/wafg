98e9d8e58467a6fb4abc0b3ba41ffe719ed73e27
Alexander Chernov <alekcander@gmail.com>
2017-08-25 19:32:28 +0100

    added test.

--- pagerule_test.go
+++ pagerule_test.go
@@ -1,4 +1,14 @@
 package wafg
 
-//import "testing"
+import (
+	"testing"
+	"github.com/alekc/wafg/matcher"
+)
 
+func TestNewSearchItem(t *testing.T){
+	mt := matcher.MatchString("bar")
+	obj := newSearchItem("foo", mt)
+	if obj.Field != "foo"{
+		t.Errorf("Object field is not foo")
+	}
+}
