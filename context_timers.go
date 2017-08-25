d2e07c7b1901faddd9c6b3715c5fcbe0657e2fa2
Alexander Chernov <alekcander@gmail.com>
2017-08-25 09:28:33 +0100

    Changed algorithm for overhead calculation. Renamed receivers

--- context_timers.go
+++ context_timers.go
@@ -9,13 +9,19 @@ type ContextTimers struct {
 	Served           time.Time
 }
 // Total elapsed time to serve this request
-func (self *ContextTimers) GetTotalTime() time.Duration {
-	diff := self.Served.Sub(self.CreatedOn)
+func (ct *ContextTimers) GetTotalTime() time.Duration {
+	diff := ct.Served.Sub(ct.CreatedOn)
 	return diff
 }
 // Time spent from the beginning of request to beginning of request vs backend server
 // Express how long does it take for us to deal with the request
-func (self *ContextTimers) GetOverhead() time.Duration {
-	diff := self.BeginRequest.Sub(self.CreatedOn)
+func (ct *ContextTimers) GetOverhead() time.Duration {
+	diff := ct.GetTotalTime() - ct.GetBackendExecTime()
+	return diff
+}
+
+// How long did it took for backend to reply to us.
+func (ct *ContextTimers) GetBackendExecTime() time.Duration{
+	diff := ct.ReceivedResponse.Sub(ct.BeginRequest)
 	return diff
 }
